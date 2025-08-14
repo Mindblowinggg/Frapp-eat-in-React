02b53f7aa29fbb780afdd5dae2e689c93d25c60f
Mindblowinggg <akahar36@gmail.com>
2025-08-14 16:12:11 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import Loginscreen from './components/loginscreen/loginscreen';
-import Splashscreen from './components/splash screen'
+import Splashscreen from './components/splash screen';
 
 function App() {
-  
+
+
+  {/*splash screen*/}
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-     <Splashscreen/>
-     <Loginscreen/>
+      {showSplash ? <Splashscreen /> : <Loginscreen />}
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/splash screen.jsx
+++ src/components/splash screen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Splashscreen = () => {
   return (
-    <div className="bg-[#142328] h-screen w-screen text-6xl   flex flex-col justify-center items-center">
+    <div className="bg-[#142328] h-screen w-screen text-6xl flex flex-col justify-center items-center">
       <h1 className="text-white font-semibold">Frapp</h1>
       <h1 className="text-[#06C167]  font-semibold">Eat</h1>
     </div>
