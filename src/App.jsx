6c3c350e411760e457df2bad87f6e8281f3f0246
Mindblowinggg <akahar36@gmail.com>
2025-08-14 15:53:00 +0530

    asd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Splashscreen from './components/splash screen'
+
+function App() {
+  
+
+  return (
+    <>
+     <Splashscreen/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/splash screen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Splashscreen = () => {
+  return (
+    <div className="bg-[#142328] h-screen w-screen text-6xl   flex flex-col justify-center items-center">
+      <h1 className="text-white font-semibold">Frapp</h1>
+      <h1 className="text-[#06C167]  font-semibold">Eat</h1>
+    </div>
+  );
+};
+export default Splashscreen;
